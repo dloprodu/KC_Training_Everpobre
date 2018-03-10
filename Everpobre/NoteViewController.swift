ca1a035d4c4f32aa6cdf09b76a4657bea5bcab82
David Lopez Rodriguez <dloprodu@gmail.com>
2018-03-10 23:46:42 +0000

    First commit

---
+++ Everpobre/NoteViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NoteViewController.swift
+//  Everpobre
+//
+//  Created by David Lopez Rodriguez on 10/03/2018.
+//  Copyright © 2018 David López Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
