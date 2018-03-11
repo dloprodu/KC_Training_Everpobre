0249454d0c803b64f257b543d6a1793941335738
David Lopez Rodriguez <dloprodu@gmail.com>
2018-03-11 15:45:32 +0000

    NoteViewByCode

--- Everpobre/AppDelegate.swift
+++ Everpobre/AppDelegate.swift
@@ -15,12 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         
         window = UIWindow()
-        let noteVC = NoteViewController()
         
-        window?.rootViewController = noteVC
+        let noteVC = NoteViewByCodeController() //NoteViewController()
+        
+        let navController = UINavigationController(rootViewController: noteVC)
+        
+        window?.rootViewController = navController
+        
         window?.makeKeyAndVisible()
         
         return true
